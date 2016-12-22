fc9fec423de593b9bffdea510233ab6be2dde497
Thomas Denglos <tdenglos@gmail.com>
2016-12-23 00:11:04 +0100

    Première lecture de fichier xlsx

--- src/index.js
+++ src/index.js
@@ -9,3 +9,26 @@ server.on('listening', () =>
 );
 
 
+var Excel = require('exceljs');
+
+// read from a file 
+var workbook = new Excel.Workbook();
+workbook.xlsx.readFile('./src/middleware/input_short.xlsx')
+    .then(function() {
+        // use workbook 
+
+        // fetch sheet by id 
+		var worksheet = workbook.getWorksheet(1);
+
+		console.log(worksheet.getCell('A1').value);
+
+
+		var dobCol = worksheet.getColumn('B');
+
+		// iterate over all current cells in this column 
+		dobCol.eachCell(function(cell, rowNumber) {
+		    console.log(cell.value);
+		});
+
+
+    });
