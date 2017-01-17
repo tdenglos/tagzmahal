48420836c831271284fb755b95fdfc61c28de3c9
Thomas Denglos <tdenglos@gmail.com>
2017-01-17 14:12:23 +0100

    login works (without SSL)

--- src/app.js
+++ src/app.js
@@ -20,8 +20,9 @@ var errorHandler = require('feathers-errors/handler');
 var authentication = require('feathers-authentication');
 const authenticationHooks = require('feathers-authentication').hooks
 // Passport Auth Strategies
+var passport = require('passport');
 var GithubStrategy = require('passport-github').Strategy;
-
+var session = require('express-session');
 // Initialize the application
 const app = feathers();
 
@@ -32,7 +33,7 @@ app.use(compress())
   .options('*', cors())
   .use(cors())
   .use(favicon( path.join(app.get('public'), 'favicon.ico') ))
-  .use('/', serveStatic( app.get('public') ))
+//  .use('/', serveStatic( app.get('public') ))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .configure(hooks())
@@ -42,26 +43,119 @@ app.use(compress())
  // .configure(middleware)
   
   // Configure feathers-authentication
-  .configure(authentication({
+/*  .configure(authentication({
     idField: 'id',
     shouldSetupSuccessRoute: true,
     github: {
       strategy: GithubStrategy,
-      'clientID': '36b9ce352600f99c1569',
+// PRODUCTION CONF
+   'clientID': '36b9ce352600f99c1569',
       'clientSecret': '190d22c079a92c0ef333b149cd0460255b12714e',
+
+// DEV CONF
+      'clientID': 'c0760198143c262b0cc3',
+      'clientSecret': '653b935888839573fc0fb42538615033f041ced0',
+// TRANSVERSAL CONF
       path: '/auth/github',
       successRedirect: '/successful_login.html'
 
     }
-  }))
+  })) */
   // Initialize a user service
   .use('/users', memory())
   //.use('/private', feathers.static(__dirname + '/private'))
-  .use('/', feathers.static(__dirname + '/public'));
+  //.use('/', feathers.static(__dirname + '/public'));
+
+
+////// Nouvelle facon en utilisant directement passport
+
+passport.use(new GithubStrategy({
+    clientID: "c0760198143c262b0cc3",
+    clientSecret: "653b935888839573fc0fb42538615033f041ced0",
+    callbackURL: "/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+
+
+// Express and Passport Session
+var session = require('express-session');
+app.use(session({secret: "essotrazef"}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  // placeholder for custom user serialization
+  // null is for errors
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // placeholder for custom user deserialization.
+  // maybe you are going to get the user from mongo by id?
+  // null is for errors
+  done(null, user);
+});
+
+app.use(function(req, res, next) {
+    console.log('private filter');
+    console.log(req.isAuthenticated());
+    console.log(req.path.indexOf('/private'));
+    if (req.isAuthenticated() == false && req.path.indexOf('/private') === 0)
+    {
+        res.redirect('/?error=not_registered_as_alpha_tester');
+    }
+    next(); 
+});
+
+app.use('/', serveStatic( app.get('public') ));
+//app.use('/', feathers.static(__dirname + '/public'));
+
+// we will call this to start the GitHub Login process
+app.get('/auth/github', passport.authenticate('github'));
+
+// GitHub will call this URL
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/successful_login.html');
+  }
+);
+
+app.get('/logout', function(req, res){
+  console.log('logging out');
+  req.logout();
+  res.redirect('/');
+});
+////////////////////
+/*
+// required for passport session
+app.use(session({
+  resave: true, 
+  saveUninitialized: true, 
+  secret: 'SOMERANDOMSECRETHERE', 
+  cookie: { maxAge: 60000 }
+}));
+
+app.use(session({
+  secret: 'secrettexthere',
+  saveUninitialized: true,
+  resave: true,
+  // using store session on MongoDB using express-session + connect
+  store: new MongoStore({
+    url: config.urlMongo,
+    collection: 'sessions'
+  })
+}));
 
 
 
-
+// Init passport authentication 
+app.use(passport.initialize());
+// persistent login sessions 
+app.use(passport.session());
+*/
 
 /*
 
@@ -98,7 +192,20 @@ app.get('/auth/github/callback',
 
 function requireLogin(req, res, next) {
   console.log('requireLogin is called');
-  authenticationHooks.verifyToken();
+  console.log (req.isAuthenticated());
+  if (req.isAuthenticated()){
+    console.log("authenticated");
+    next();
+  }else{
+    console.log("not authenticated");
+    res.redirect('/?error=not_registered_as_alpha_tester');
+  }
+
+
+  /*
+  console.log(req.feathers.token);
+  console.log(authenticationHooks.verifyToken());
+  console.log(authenticationHooks.restrictToAuthenticated());
   if ( authenticationHooks.restrictToAuthenticated() instanceof Error ) {
     console.log('error');
     res.redirect('/');
@@ -106,7 +213,7 @@ function requireLogin(req, res, next) {
     console.log('success');
     next();
   }
-
+*/
 
 
 
@@ -133,6 +240,20 @@ app.all('/private/home', requireLogin, function(req, res, next){
 );
 
 
+app.all('/private/dashboard', requireLogin, function(req, res, next){
+  res.sendFile(path.resolve(__dirname,'..', 'public', 'private', 'dashboard.html'));
+  }
+);
+
+app.all('/private/dropzone', requireLogin, function(req, res, next){
+  res.sendFile(path.resolve(__dirname,'..', 'public', 'private', 'dropzone.html'));
+  }
+);
+
+app.all('/private/newrun', requireLogin, function(req, res, next){
+  res.sendFile(path.resolve(__dirname,'..', 'public', 'private', 'newrun.html'));
+  }
+);
 
 // Automatically apply the `requireLogin` middleware to all
 // routes starting with `/admin`
@@ -148,10 +269,6 @@ app.get("/admin/posts", function(req, res) {
   // ensured that the user is logged in
 });
 */
-
-
-
-
 
 
 
